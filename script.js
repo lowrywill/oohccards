42f2a791d94a23e9f5d4184a50416f271d43aaec
lowrywill <67927513+lowrywill@users.noreply.github.com>
2023-03-23 17:00:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
-// Get the current sum of the sliders
-var sum = 0;
-for (var i = 0; i < 3; i++) {
-  sum += parseInt(document.getElementById("slider" + i).value);
+// Get the sliders and sum element
+const slider1 = document.getElementById("stat-slider-1");
+const slider2 = document.getElementById("stat-slider-2");
+const slider3 = document.getElementById("stat-slider-3");
+const sumElement = document.getElementById("slider-sum");
+
+// Update the sum when the sliders change
+function updateSum() {
+  const sum = parseInt(slider1.value) + parseInt(slider2.value) + parseInt(slider3.value);
+  sumElement.innerHTML = sum;
 }
 
-// Get the first card from the first deck
-var card1 = document.getElementById("deck1").firstChild;
-
-// Get the first card from the second deck
-var card2 = document.getElementById("deck2").firstChild;
-
-// Reveal the first card
-card1.style.backgroundColor = "red";
-
-// Discard the second card
-document.getElementById("deck2").removeChild(card2);
-
-// Ask the player a question
-var question = "What is the color of card 1?";
-
-// Get the player's answer
-var answer = document.getElementById("questions").value;
-
-// Check if the player's answer is correct
-if (answer === "red") {
-  // The player got the question correct
-  document.getElementById("questions").value = "";
-} else {
-  // The player got the question wrong
-  document.getElementById("questions").value = "Incorrect. The color of card 1 is red.";
-}
+slider1.addEventListener("input", updateSum);
+slider2.addEventListener("input", updateSum);
+slider3.addEventListener("input", updateSum);
